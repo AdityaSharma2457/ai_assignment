fb352e8d2ad2d0fe321d76832f01e7e3312759bb
Aditya Sharma <sharmadi.kota@gmail.com>
2026-09-04 13:42:52 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import time
+st.write("adity")
+st.checkbox("puu")
+st.checkbox('Yes')
+st.button('Click Me')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick a fruit', ['Apple', 'Banana', 'Orange'])
+st.multiselect('Choose a planet', ['Jupiter', 'Mars', 'Neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0, 50)
+
+st.number_input('Pick a number', 0, 10)
+st.text_input('Email address')
+st.date_input('Traveling date')
+st.time_input('School time')
+st.text_area('Description')
+st.file_uploader('Upload a photo')
+st.color_picker('Choose your favorite color')
+
+st.balloons()  # Celebration balloons
+st.progress(100)  # Progress barwith 
+st.spinner('Wait for it...')
+  # Simulating a process delay
